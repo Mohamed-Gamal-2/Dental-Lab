38ab40fbc514af9916057c99b19bab40a90fb366
Mohamed-Gamal-2 <141522329+Mohamed-Gamal-2@users.noreply.github.com>
2023-10-27 18:48:50 +0200

    Create Dentists Module router also, createing  controller skeleton

---
+++ Modules/Dentists/Dentists.Controller.js
@@ -0,0 +1,38 @@
+import DentistsModel from "../../Database/Models/Dentists.Model.js";
+function getAllDentists(req, res) {
+  try {
+    res.status(200).json({ status: "Success", data: "Get all" });
+  } catch (error) {
+    res.status(400).json({ status: "Fail", message: error });
+  }
+}
+function deleteDentist(req, res) {
+  try {
+    res.status(200).json({ status: "Success", data: "Delete" });
+  } catch (error) {
+    res.status(400).json({ status: "Fail", message: error });
+  }
+}
+function updateDentist(req, res) {
+  try {
+    res.status(200).json({ status: "Success", data: "Update" });
+  } catch (error) {
+    res.status(400).json({ status: "Fail", message: error });
+  }
+}
+function getDentist(req, res) {
+  try {
+    res.status(200).json({ status: "Success", data: "Get Dentist" });
+  } catch (error) {
+    res.status(400).json({ status: "Fail", message: error });
+  }
+}
+function addDentist(req, res) {
+  try {
+    res.status(200).json({ status: "Success", data: "add" });
+  } catch (error) {
+    res.status(400).json({ status: "Fail", message: error });
+  }
+}
+
+export { getAllDentists, deleteDentist, updateDentist, getDentist, addDentist };
