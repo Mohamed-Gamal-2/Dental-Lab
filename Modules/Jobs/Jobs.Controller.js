440f245a2a4455428aa4a58a1ec985dc820edcd0
Ellithy <engmohamedellithy13@gmail.com>
2023-10-27 19:12:56 +0200

    add Jobs router + controller

---
+++ Modules/Jobs/Jobs.Controller.js
@@ -0,0 +1,46 @@
+function allJobs (req,res){
+    try {
+        res.status(200).json({status:"success", data:"all Jobs"})
+    } catch (error) {
+        res.status(400).json({status:"fail",message:"error"})
+    }
+}
+function addJob(req, res) {
+  try {
+    res.status(200).json({ status: "success", data: " job added" });
+  } catch (error) {
+    res.status(400).json({ status: "fail", message: "error" });
+  }
+}
+function deleteJob(req, res) {
+  try {
+    res.status(200).json({ status: "success", data: "Job deleted" });
+  } catch (error) {
+    res.status(400).json({ status: "fail", message: "error" });
+  }
+}
+
+function updateJob(req, res) {
+  try {
+    console.log("all Jobs");
+    res.status(200).json({ status: "success", data: "Job updated" });
+  } catch (error) {
+    res.status(400).json({ status: "fail", message: "error" });
+  }
+}
+
+function getJob(req, res) {
+  try {
+    console.log("all Jobs");
+    res.status(200).json({ status: "success", data: "all Jobs" });
+  } catch (error) {
+    res.status(400).json({ status: "fail", message: "error" });
+  }
+}
+export{
+    allJobs,
+    addJob,
+    deleteJob,
+    updateJob,
+    getJob
+}
