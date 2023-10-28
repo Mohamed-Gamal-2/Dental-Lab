c6d6189dd787eadb66dd1d49f1d9e621d2705d39
Abdelkader Elsisy <66458422+abdelkader470@users.noreply.github.com>
2023-10-28 23:40:13 +0200

    update in staff controller

--- Modules/Staff/Staff.Controller.js
+++ Modules/Staff/Staff.Controller.js
@@ -25,35 +25,41 @@ const getOneStaff = asyncHandler(async (req, res, next) => {
   }
 });
 
-const addStaff = asyncHandler(async (req, res) => {
+const addStaff = asyncHandler(async (req, res, next) => {
   const { ssn, name, jobTitle, age, salary, phone, email, gender } = req.body;
-  const staff = await staffModel.create({
-    ssn,
-    name,
-    jobTitle,
-    age,
-    salary,
-    phone,
-    email,
-    gender,
-  });
-  res.status(201).json({ status: "success", data: staff });
-});
-
-const updataStaff = asyncHandler(async (req, res, next) => {
-  const { id } = req.params;
-  const { name, salary, jobTitle } = req.body;
-  const staff = await staffModel.findOneAndUpdate(
-    { _id: id },
-    { name, salary, jobTitle },
-    { new: true }
-  );
-  if (!staff) {
-    return next(new ApiError("no staff for this id ${id}", 404));
+  const creatorId = req.decodedToken.id;
+  const admin = await adminModel.findById(creatorId);
+  if (admin) {
+    const staff = await staffModel.create({
+      createdBy: creatorId,
+      ...req.body,
+    });
+    res.status(201).json({ status: "success", data: staff });
   } else {
-    res.status(200).json({ status: "success", data: staff });
+    return next(new ApiError("invaild token", 401));
   }
 });
+
+const updataStaff = async (req, res, next) => {
+  try {
+    const creatorId = req.decodedToken.id;
+    const admin = await adminModel.findById(creatorId);
+    if (admin) {
+      const { id } = req.params;
+      const { name, salary, jobTitle } = req.body;
+      const staff = await staffModel.findOneAndUpdate(
+        { _id: id },
+        { name, salary, jobTitle },
+        { new: true }
+      );
+      res.status(200).json({ status: "success", data: staff });
+    } else {
+      return next(new ApiError("no staff for this id ${id}", 404));
+    }
+  } catch (error) {
+    res.status(400).json({ status: "fail", message: error });
+  }
+};
 
 const deleteStaff = asyncHandler(async (req, res, next) => {
   const { id } = req.params;
