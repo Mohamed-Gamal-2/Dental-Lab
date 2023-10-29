44db3f95212a4946cdc8641cac33bc645b3dc2c9
Ellithy <engmohamedellithy13@gmail.com>
2023-10-29 10:51:12 +0200

    Job controller +update Job validation

--- Modules/Jobs/Jobs.Controller.js
+++ Modules/Jobs/Jobs.Controller.js
@@ -1,45 +1,132 @@
 import adminModel from "../../Database/Models/Admin.Model.js";
 import jobModel from "../../Database/Models/Jobs.Model.js";
+import jwt from "jsonwebtoken";
 
 
-
-
-function allJobs (req,res){
+// get all Jobs
+async function allJobs (req,res){
     try {
-        res.status(200).json({status:"success", data:"all Jobs"})
+       let getallJobs = await jobModel.find();
+        res.status(200).json({ status: "success",  getallJobs });
     } catch (error) {
         res.status(400).json({status:"fail",message:"error"})
     }
 }
-function addJob(req, res) {
-  try {
-    res.status(200).json({ status: "success", data: " job added" });
+
+// add Job
+async function addJob(req, res) {
+  try{
+    const {serial}=req.body
+    const decoded = jwt.verify(req.headers.token, "bl7 5ales");
+    const { id: createdBy } = decoded;
+    const JobFounded = await jobModel.findOne({serial});
+    if(JobFounded){
+      return res.status(401).json({ message: " This Serial is already exist" })
+    }else{
+      const adminFounded = await adminModel.findById(createdBy)
+    if(!adminFounded){
+      console.log(" Not Allowed , You aren't Admin")
+      return res.status(401).json({ message: " Not Allowed , You aren't Admin" });
+    }else{
+          let newJob = await jobModel.insertMany({
+            ...req.body,
+            createdBy,
+          });
+
+      res
+        .status(200)
+        .json({
+          status: "success",
+          message: "A New Job Added successfully",
+          newJob
+        });
+    }}
   } catch (error) {
-    res.status(400).json({ status: "fail", message: "error" });
-  }
-}
-function deleteJob(req, res) {
-  try {
-    res.status(200).json({ status: "success", data: "Job deleted" });
-  } catch (error) {
-    res.status(400).json({ status: "fail", message: "error" });
+    res.status(400).json({ status: "fail", message: "error",error });
   }
 }
 
-function updateJob(req, res) {
+
+// Delete Job
+async function deleteJob(req, res) {
   try {
-    console.log("all Jobs");
-    res.status(200).json({ status: "success", data: "Job updated" });
+     let { id } = req.params;
+     const decoded = jwt.verify(req.headers.token, "bl7 5ales");
+     const { id: createdBy } = decoded;
+       const adminFounded = await adminModel.findById(createdBy)
+    if(!adminFounded){
+      console.log(" Not Allowed , You aren't Admin")
+      return res.status(401).json({ message: " Not Allowed , You aren't Admin" });
+    }else{
+      let jobDeleted = await jobModel.findByIdAndDelete(id);
+      let allJobs = await jobModel.find();
+      if (jobDeleted) {
+        res.status(201).json({ massage: "Job deleted", allJobs });
+      } else {
+        res.status(400).json({ massage: "Job not found" });
+      }}
   } catch (error) {
     res.status(400).json({ status: "fail", message: "error" });
   }
 }
+// Update Job
+async function updateJob(req, res) {
+  try{
+    const { id } = req.params;
+    const decoded = jwt.verify(req.headers.token, "bl7 5ales");
+    const { id: createdBy } = decoded;
+    const adminFound = await adminModel.findById(createdBy);
+    if (!adminFound) {
+      return res
+        .status(401)
+        .json({ message: "Not Allowed, You are not an Admin" });
+    }else{
 
-function getJob(req, res) {
+      const updatedJob = await jobModel.findByIdAndUpdate(
+        id,
+        {
+          serial: req.body.serial,
+          pationName: req.body.pationName,
+          doctorId: req.body.doctorId,
+          typeOfWork: req.body.typeOfWork,
+          teethNumber: req.body.teethNumber,
+          shade: req.body.shade,
+          deadLine: req.body.deadLine,
+          price: req.body.price,
+          tryIn: req.body.tryIn,
+          materialOfPorclain: req.body.materialOfPorclain, // Corrected the field name
+        },
+        { new: true }
+        );
+        
+        if (updatedJob) {
+          return res.status(200).json({ message: "Job updated", updatedJob });
+        } else {
+          return res.status(404).json({ message: "Job not found" });
+        }
+      }
+      } catch (error) {
+    return res
+      .status(500)
+      .json({ status: "fail", message: "An error occurred",error });
+  }
+}
+
+// get a specific Job
+async function getJob(req, res) {
   try {
-    console.log("all Jobs");
-    res.status(200).json({ status: "success", data: "all Jobs" });
-  } catch (error) {
+    let{id}=req.params
+     const decoded = jwt.verify(req.headers.token, "bl7 5ales");
+    const { id: createdBy } = decoded;
+    const adminFound = await adminModel.findById(createdBy);
+    if (!adminFound) {
+      return res
+        .status(401)
+        .json({ message: "Not Allowed, You are not an Admin" });
+    }else{
+    let getJob = await jobModel.findById(id);
+    res.status(200).json({ status: "success", getJob });
+  }} catch (error) {
     res.status(400).json({ status: "fail", message: "error" });
   }
 }
--- middleware/validation.js
+++ middleware/validation.js
@@ -1,6 +1,6 @@
-const validation = (schema,source) => {
+const validation = (schema) => {
     return   (req, res, next) => {
-           let { error } = schema.validate(req[source]);
+           let { error } = schema.validate(req.body);
            if (error) {
                res.status(401).json({ message: "Err", error })
            } else {
