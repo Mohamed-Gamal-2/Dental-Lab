340725250279bd681ff54f855e89467cd0443ece
Abdelkader Elsisy <66458422+abdelkader470@users.noreply.github.com>
2023-11-13 22:30:31 +0200

    update validation

--- Modules/Staff/Staff.Validation.js
+++ Modules/Staff/Staff.Validation.js
@@ -2,7 +2,9 @@ import Joi from "joi";
 
 const staffRegisterValidationSchema = Joi.object({
   createdBy: Joi.string(), // You can specify ObjectId validation here
-  ssn: Joi.string().pattern(/^\d{14}$/),
+  ssn: Joi.string()
+    .required()
+    .pattern(/^\d{14}$/),
   name: Joi.string().required().min(3).max(30),
   jobTitle: Joi.string().required().min(3).max(30),
   age: Joi.number().integer().required().max(80).min(15),
@@ -11,20 +13,22 @@ const staffRegisterValidationSchema = Joi.object({
     .required()
     .pattern(/01(0|1|2|5)[0-9]{8}$/),
   email: Joi.string()
-    .email()
+    .required()
     .pattern(/^[a-z]+([a-z]|[0-9]|_|.)*@(gmail|yahoo|hotmail).(com)$/),
   gender: Joi.string().valid("male", "female").required(),
   role: Joi.string().valid("user", "admin").default("user"),
 });
 
 const staffUpdateValidationSchema = Joi.object({
-  ssn: Joi.number().integer(),
-  name: Joi.string(),
-  jobTitle: Joi.string(),
-  age: Joi.number().integer(),
-  salary: Joi.number(),
-  phone: Joi.string(),
-  email: Joi.string().email(),
+  ssn: Joi.string().pattern(/^\d{14}$/),
+  name: Joi.string().min(3).max(30),
+  jobTitle: Joi.string().min(3).max(30),
+  age: Joi.number().integer().max(80).min(15),
+  salary: Joi.number().max(30000),
+  phone: Joi.string().pattern(/01(0|1|2|5)[0-9]{8}$/),
+  email: Joi.string().pattern(
+    /^[a-z]+([a-z]|[0-9]|_|.)*@(gmail|yahoo|hotmail).(com)$/
+  ),
   gender: Joi.string().valid("male", "female"),
   role: Joi.string().valid("user", "admin").default("user"),
 });
